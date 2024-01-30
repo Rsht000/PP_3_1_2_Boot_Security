2a14d0830c77a7a55c340cb8b001cbfaeee508d0
админ <lodbrokragnar548@gmail.com>
2024-01-30 16:29:46 +0300

    Security

--- src/main/java/ru/kata/spring/boot_security/demo/Controllers/AdminController.java
+++ src/main/java/ru/kata/spring/boot_security/demo/Controllers/AdminController.java
@@ -1,15 +1,24 @@
 package ru.kata.spring.boot_security.demo.Controllers;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import ru.kata.spring.boot_security.demo.Service.UserServiceImpl;
+
+import java.security.Principal;
 
 @Controller
 @RequestMapping("/admin")
 public class AdminController {
+    private final UserServiceImpl userService;
 
+    public AdminController(UserServiceImpl userService) {
+        this.userService = userService;
+    }
     @GetMapping()
-    public String showAdminGeneralPage() {
+    public String showAdminGeneralPage(Model model, Principal principal) {
+        model.addAttribute("login" , userService.findByUserName(principal.getName()));
         return "/admin";
     }
 }
--- src/main/java/ru/kata/spring/boot_security/demo/Controllers/UserController.java
+++ src/main/java/ru/kata/spring/boot_security/demo/Controllers/UserController.java
@@ -1,14 +1,24 @@
 package ru.kata.spring.boot_security.demo.Controllers;
 
+
 import org.springframework.stereotype.Controller;
+
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import ru.kata.spring.boot_security.demo.Service.UserServiceImpl;
+
+import java.security.Principal;
 
 @Controller
 public class UserController {
+    private final UserServiceImpl userService;
 
-
+    public UserController(UserServiceImpl userService) {
+        this.userService = userService;
+    }
     @GetMapping("/user")
-    public String userForm() {
+    public String userForm(Model model, Principal principal) {
+        model.addAttribute("login",userService.findByUserName(principal.getName()));
         return "/user";
     }
 }
